1016d49f691578e9675ad86dbe66f792f549f8ee
Alex Hapgood <ach1@vt.edu>
2024-01-15 21:31:08 -0500

    Add knex dependency and update docker-compose.yaml

---
+++ node/knexfile.js
@@ -0,0 +1,35 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: 'db',
+      database: 'db123',
+      user:     'user123',
+      password: 'password123'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
+  }
+
+};
