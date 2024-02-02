ee47242a9e4026414844d9a08dad5cb5fcd1e40e
Alex Hapgood <ach1@vt.edu>
2024-02-01 20:08:11 -0500

    Add initial tables and seed data for mfgs, gear, and recipes (db re-work WIP 1)

--- node/src/knex/migrations/20240125011533_initial_tables.ts
+++ node/src/knex/migrations/20240125011533_initial_tables.ts
@@ -3,15 +3,49 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema
-        .createTable('recipes', function (table) {
+        .createTable('mfgs', function (table: Knex.CreateTableBuilder) {
+            table.increments('id');
+            table.string('name', 255).notNullable();
+            table.string('description', 1000).notNullable();
+            table.string('country_of_origin', 255).notNullable();
+            table.string('site_url', 255);
+            table.string('logo_url', 255);
+        })
+        .createTable('gear', function (table: Knex.CreateTableBuilder) { 
+            table.increments('id');
+            table.integer('mfg_id').notNullable().references('id').inTable('mfgs');
+            table.string('name', 255).notNullable();
+            table.string('description', 1000).notNullable();
+            table.string('photo_url', 255);
+            table.enu('type', ['grinder', 'brewer', 'kettle', 'scale', 'other']).notNullable();
+            table.boolean('grinder_stepped').defaultTo(true);
+            table.enu('grinder_burr_type', ['conical', 'flat', 'other']).defaultTo('conical');
+            table.float('grinder_min_gradation').defaultTo(0);
+            table.float('grinder_max_gradation').defaultTo(9);
+            table.integer('grinder_total_revolutions');
+        })
+        .createTable('recipes', function (table: Knex.CreateTableBuilder) {
             table.increments('id');
             table.integer('version').notNullable().defaultTo(1);
-            table.boolean('is_public').defaultTo(false);
+            table.boolean('is_pub').defaultTo(false);
             table.timestamp('created_at').defaultTo(knex.fn.now());
             table.string('name', 255).notNullable();
             table.string('description', 1000).notNullable();
-            table.string('steps', 1000).notNullable();
+            table.specificType('steps', 'text ARRAY').defaultTo([]);
+            table.specificType('step_additional_notes', 'text ARRAY').defaultTo([]);
+            table.specificType('step_start_times', 'integer ARRAY').defaultTo([0]);
+            table.specificType('step_lengths', 'integer ARRAY').defaultTo([1000]);
+            table.specificType('step_water_volumes', 'integer ARRAY').defaultTo([]);
+            table.integer('total_time').defaultTo(6000);
+            table.integer('water_temp_c');
+            table.integer('dry_coffee_weight_g');
+            table.integer('water_vol_ml');
             table.integer('owner_auth0_id');
+        })
+        .createTable('recipes_gear', function (table: Knex.CreateTableBuilder) {
+            table.integer('recipe_id').notNullable().references('id').inTable('recipes');
+            table.integer('gear_id').notNullable().references('id').inTable('gear');
+            table.primary(['recipe_id', 'gear_id']);
         });
 }
 
--- node/src/knex/seeds/initial_data.ts
+++ node/src/knex/seeds/initial_data.ts
@@ -3,38 +3,70 @@ import { Knex } from "knex";
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
     await knex("recipes").del();
-    await knex("users").del();
+    await knex("gear").del();
+    await knex("mfgs").del();
 
     // Inserts seed entries
-    await knex("users").insert([
-        {email: 'alex@hapgood.me', password:'password123'},
-        {email: 'garrett@moore.me', password: 'password456'}
-    ])
+    await knex("mfgs").insert([
+        {
+            name: 'Option-O',
+            description: 'Option-O is a small company that makes high quality coffee grinders.',
+            country_of_origin: 'AUS',
+            site_url: 'https://www.option-o.com/',
+            logo_url: 'https://images.squarespace-cdn.com/content/v1/5b125642ec4eb782ce4f9947/1590805199369-I6XQ3B1LLKGB5SGJ27SI/%E8%B5%84%E6%BA%90+4.png',
+        },
+        {
+            name: 'Bodum',
+            description: 'this is a test mfg entry #2!',
+            country_of_origin: 'USA',
+            site_url: 'https://www.google.com',
+            logo_url: 'https://www.google.com',
+        },
+        {
+            name: 'Hario',
+            description: 'this is a test mfg entry #3!',
+            country_of_origin: 'USA',
+            site_url: 'https://www.google.com',
+            logo_url: 'https://www.google.com',
+        },
+        {
+            name: 'Stagg',
+            description: 'this is a test mfg entry #4!',
+            country_of_origin: 'USA',
+            site_url: 'https://www.google.com',
+            logo_url: 'https://www.google.com',
+        }
+    ]);
+    await knex("gear").insert([
+        {
+            mfg_id: 1,
+            name: 'Option-O Lagom Mini',
+            description: 'The Lagom Mini is Option-O\'s smallest offering, suitable for home use.',
+            photo_url: 'https://images.squarespace-cdn.com/content/v1/5b125642ec4eb782ce4f9947/d1f14d05-4b2d-4335-9a6d-e98576940f9b/DSC01961.jpg',
+            type: 'grinder',
+            grinder_stepped: false,
+            grinder_burr_type: 'conical',
+            grinder_min_gradation: 0.0,
+            grinder_max_gradation: 9.9,
+            grinder_total_revolutions: 5,
+        }
 
-    await knex("users").select('*').then((val) => {
-        let userIdsForSeed: number[] = [];
-        val.forEach((user, idx):void => {
-            userIdsForSeed[idx] = user.id;
-        });
-        return userIdsForSeed;
-        }).then(async(userIdsForSeed) => {    
-            await knex("recipes").insert([
-                {
-                    name: 'test recipe 1!',
-                    description: 'this is a test recipe entry #1!',
-                    steps: '1. grind beans. 2. make coffee. 3. Drink coffee',
-                    owner_id: `${userIdsForSeed[0]}`,
-                    version: 1,
-                },
-                {
-                    name: 'test recipe 2!',
-                    description: 'this is a test recipe entry #2!',
-                    steps: '1. grind beans BETTER. 2. make coffee BETTER. 3. Drink coffee MORE',
-                    owner_id: `${userIdsForSeed[1]}`,
-                    version: 1,
-                }
-            ]);
-        })
+    ]);
+    await knex("recipes").insert([
+        {
+            name: 'test recipe 1!',
+            description: 'this is a test recipe entry #1!',
+            steps: '1. grind beans. 2. make coffee. 3. Drink coffee',
+            version: 1,
+        },
+        {
+            name: 'test recipe 2!',
+            description: 'this is a test recipe entry #2!',
+            steps: '1. grind beans BETTER. 2. make coffee BETTER. 3. Drink coffee MORE',
+            version: 1,
+        }
+    ]);
+        
 
    
 };
